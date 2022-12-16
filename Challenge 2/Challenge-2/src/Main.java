b0c8ff2fa5e4983d983a0b7be454fdd530802303
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-12-16 23:13:09 +0530

    Challenge 2 updated

--- Challenge 2/Challenge-2/src/Main.java
+++ Challenge 2/Challenge-2/src/Main.java
@@ -2,54 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        QueueX queue1 = new QueueX(10);
-        QueueX queue2 = new QueueX(10);
-        StackX stack = new StackX(10);
+        int SIZE;
+        int extra=1;
         String input;
-        Boolean output=false;
         char[] inputArray;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Input : ");
         input = sc.nextLine();
         inputArray = input.toCharArray();
+        SIZE=inputArray.length;
 
-        //add opening and closing brackets to separate queues
-        for(int i=0; i < inputArray.length; i++){
+        StackX stack = new StackX(SIZE);
+
+        for(int i=0; i<SIZE ; i++){
             if(inputArray[i]=='{' || inputArray[i]=='[' || inputArray[i]=='('){
-                queue1.insert(inputArray[i]);
-            }
-            if(inputArray[i]=='}' || inputArray[i]==']' || inputArray[i]==')'){
-                queue2.insert(inputArray[i]);
+                stack.push(inputArray[i]);
+            } else {
+                if(stack.isEmpty()){
+                    extra = 1; //extra closing bracket
+                    break;
+                }
+                char openBracket = stack.pop();
+                if(inputArray[i] == ')' && openBracket!= '(' || inputArray[i] == '}' && openBracket!= '{' || inputArray[i] == ']' && openBracket!= '['){
+                    extra=1;
+                    break;
+                }else{
+                    extra=0;
+                }
             }
         }
 
-        //for reverse the pattern in queue 2-{[()]} like patterns
-//        while(!queue2.isEmpty()){
-//            stack.push(queue2.remove());
-//        }
-//        while(!stack.isEmpty()){
-//            queue2.insert(stack.pop());
-//        }
-
-        //check whether number of opening brackets and closing brackets are same
-        if(queue1.getnItems()==queue2.getnItems()){
-            //check whether all opening bracket has a closing bracket
-            for(int j=0; j < queue1.getnItems(); j++){
-                if(queue1.remove()=='{' && queue2.remove()=='}'){
-                    output=true;
-                }else if(queue1.remove()=='[' && queue2.remove()==']'){
-                    output=true;
-                }else if(queue1.remove()=='(' && queue2.remove()==')'){
-                    output=true;
-                }else{
-                    output=false;
-                    break;
-                }
-            }
-            System.out.println("Output : " + output);
+        //check
+        if(stack.isEmpty() && extra == 0){
+            System.out.println("output : "+true );
         }else{
-            System.out.println("Output : " + false);
+            System.out.println("output : "+false);
         }
 
 
